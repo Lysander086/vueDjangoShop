feadf316ef6d2808d2c12e296530d598b9bf1b99
z <z@example.com>
2020-10-21 19:38:19 +0800

    c5-2 图片测试httpResponse成功

--- apps/goods/views_base.py
+++ apps/goods/views_base.py
@@ -2,27 +2,24 @@ from django.views.generic import View
 from goods.models import Goods
 
 
-class GoodsListView(View): # 重载view完成列表返回
+class GoodsListView(View):  # 重载view完成列表返回
     def get(self, request):
         # 通过django的view实现商品列表页
         json_list = []
         # 获取所有商品
-        goods = Goods.objects.all()
-        for good in goods:
-            json_dict = {}
-            # 获取商品的每个字段，键值对形式
-            json_dict['name'] = good.name
-            json_dict['category'] = good.category.name
-            json_dict['market_price'] = good.market_price
-            json_list.append(json_dict)
+        goods = Goods.objects.all()[:10]
 
+        from django.forms.models import model_to_dict
+        # for good in goods:
+        #     json_dict = model_to_dict(good)
+        #     json_list.append(json_dict)
+
+        from django.core import serializers
         import json
-        # from django.core import serializers
-        # from django.http import JsonResponse
-        #
-        # json_data = serializers.serialize('json', goods)
-        # json_data = json.loads(json_data)
-        # # In order to allow non-dict objects to be serialized set the safe parameter to False.
-        # return JsonResponse(json_data, safe=False)
-        from django.http import HttpResponse
-        return HttpResponse(json.dumps(json_list), content_type="application/json")
+
+        json_data = serializers.serialize("json", goods)
+        # json_data = json.loads(json_data) # pairz1 start
+
+        from django.http import HttpResponse, JsonResponse
+        # return HttpResponse(json.dumps(json_data), content_type="application/json") # pairz1 end
+        return HttpResponse(json_data, content_type="application/json")
